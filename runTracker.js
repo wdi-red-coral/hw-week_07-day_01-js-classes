f4d84f124f0a77f4b988cdb7f9207fa28b029d52
hudabz <huda_bz@hotmail.com>
2019-06-24 00:58:13 +0300

    submitting hw week 7 day 1 It was an easy HW, enjoyed solved it

---
+++ runTracker.js
@@ -0,0 +1,38 @@
+class Tracker {
+
+     arrDis =[]; 
+     arrTime = []; 
+    constructor(name, email){
+        this.name = name; 
+        this.email = email; 
+      }
+    
+      addRun(dateTime, distance, timeToken) {
+        this.dateTime = dateTime; 
+        this.distance = distance; 
+        this.timeToken = timeToken;  
+        this.arrDis.push(this.distance);
+        this.arrTime.push(this.timeToken);  
+      }
+      
+      totalDistance () {
+          this.distance += this.distance; 
+      }
+
+      totalTokenTime () {
+        this.timeToken += this.timeToken; 
+    }
+     longestistance (){
+    let largest = this.arrDis.reduce((larg, curr)=> Math.max(larg, curr))
+    return largest; 
+  }  
+
+     avgSpeed () {
+ let totalTime= this.arrTime.reduce((time, curr)=> time + curr)
+ let totalDis= this.arrDis.reduce((dis, curr)=> dis + curr) 
+
+       return totalDis / totalTime; 
+         
+    }
+
+} 
