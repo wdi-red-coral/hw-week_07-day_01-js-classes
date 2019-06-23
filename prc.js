8042309f35cbb72acd011390a730136983c45c63
Nader <na511dr@hotmail.com>
2019-06-23 21:58:37 +0300

    Final version of HW

--- prc.js
+++ prc.js
@@ -2,6 +2,7 @@ class runTracker {
     constructor(name, email){
         this.name = name;
         this.email = email;
+        this.time = 0;
         this.distance = [];
         this.date = [];
     }
@@ -9,6 +10,8 @@ class runTracker {
     addRun(date, distance, time){
         this.distance.push(distance);
         this.date.push(date);
+        this.time = time;
+        console.log('On '+ date +', I have run: ' + distance + ' meters. It took ' + time + ' minutes');
     }
 
     totalDistance(){
@@ -16,20 +19,17 @@ class runTracker {
     }
 
     longestDistance(){
-        return this.distance.reduce((max, current) => {
-            if(max < current){
-                return current
-            }
-            else{
-                return max
-            }
-        })
+        return this.distance.reduce((max, current) => (max < current) ? current : max)
+
     }
 }
 
 const nader = new runTracker('Nader', 'me@althubaity.com');
 
 nader.addRun("10-10-2010", 10, 30);
-nader.addRun("12-12-2012", 5, 20);
+nader.addRun("12-12-2011", 12, 200);
+nader.addRun("12-12-2010", 50, 19);
+nader.addRun("12-12-2060", 91, 88);
+
 console.log(nader.totalDistance());
 console.log(nader.longestDistance());
