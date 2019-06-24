dc882092e856c63a2e036f49b7942675f2202557
ksaninja <salha.aqeel@gmail.com>
2019-06-24 09:53:55 +0300

    still need to be working on it

---
+++ RunTracker.js
@@ -0,0 +1,41 @@
+class RunTracker {
+
+    constructor (userinfo){
+
+        this.name=name
+        this.email=email
+        this.userinfo=[]
+        this.addrun=[]
+        this.date=date
+        this.time=0
+        this.distance=0
+        this.addrun=0
+        this.distancearr=[]
+    }
+
+    runner(n){
+
+        this.userinfo.push(name + email)
+        this.addrun.push(date , distance, time)
+        }
+
+
+    totaldistance(distance){
+       
+        this.distance=this.distance+distance
+        this.distancearr.push()
+    }
+
+    longestDistance(){
+
+        this.distancearr.filter()
+
+    }
+
+    avaregspeed(){
+
+        console.log()
+    }
+
+
+}
